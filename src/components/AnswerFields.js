17c981b822c1fe2bf5c4f408e7683a3a7132dabe
Bobby <bobby077joe@gmail.com>
2017-12-22 17:43:16 -0800

    added scores

--- src/components/AnswerFields.js
+++ src/components/AnswerFields.js
@@ -1,11 +1,14 @@
 import React from 'react'
 
 const answerFields = (props) => (
-    <div>
-        <form onSubmit={props.clicked} refs="form">        
+    <div className="container">
+        <form onSubmit={props.clicked} refs="form">
+        <div className="form-group">
         <h1>Enter Your Answer</h1>
-        <input type="text" onChange={props.changed}/>
-        <button type="submit">Submit</button>
+        <input className='form-control' type="text" onChange={props.changed}/>
+        </div>
+        <button className="btn btn-success" type="submit">Answer</button>
+        
         </form>
     </div>
 )
--- src/containers/BaseQuiz.js
+++ src/containers/BaseQuiz.js
@@ -1,81 +1,94 @@
-import React from 'react';
+import React from "react";
 
-import AnswerFields from '../components/AnswerFields'
-import TopDisplay from '../components/TopDisplay'
+import AnswerFields from "../components/AnswerFields";
+import TopDisplay from "../components/TopDisplay";
 
 class BaseQuiz extends React.Component {
-    state = {
-        currentAnswer: null,
-        currentIndex: 0,
-        answerCorrectly: 0,
-        quiz: [
-            {question: 'Type 1', answer: '1'},
-            {question: 'Type 2', answer: '2'},
-            {question: 'Type 3', answer: '3'}
-        ]
+  state = {
+    currentAnswer: null,
+    currentIndex: 0,
+    answerCorrectly: 0,
+    quiz: [
+      { question: "What does the R in MERN stack stand for?", answer: "react" },
+      {
+        question: "What language is behind the Django framework?",
+        answer: "python"
+      },
+      {
+        question: "What language is behind the Node framework",
+        answer: "javascript"
+      }
+    ]
+  };
+
+  answerChangedHandler = event => {
+    let newAnswer = event.target.value;
+    this.setState({ currentAnswer: newAnswer });
+  };
+
+  answerSubmit = e => {
+    e.preventDefault();
+    e.target.reset();
+    const quiz = this.state.quiz;
+    let currentIndex = this.state.currentIndex;
+    let answerCorrectly = this.state.answerCorrectly;
+    const currentAnswer = this.state.currentAnswer;
+
+    if (currentAnswer.trim().toLowerCase() === quiz[currentIndex].answer) {
+      console.log("right");
+      this.setState({
+        currentIndex: (currentIndex += 1),
+        answerCorrectly: (answerCorrectly += 1)
+      });
+      console.log(currentIndex);
+    } else {
+      console.log("wrong");
+      this.setState({ currentIndex: (currentIndex += 1) });
     }
 
-    answerChangedHandler = (event) => {
-        let newAnswer = event.target.value
-        this.setState({currentAnswer: newAnswer})        
+    this.setState({ currentAnswer: null });
+  };
+
+  render() {
+    const quiz = this.state.quiz;
+    const currentIndex = this.state.currentIndex;
+    const answerCorrectly = this.state.answerCorrectly;
+
+    let message = null;
+    // If a perfect score is achieved Perfect is displayed
+    if (answerCorrectly === quiz.length) {
+      message = <TopDisplay title="Perfect Score" />;
+
+      // If the current index is less than the quiz length,
+      // the quiz continues
+    } else if (currentIndex < quiz.length) {
+      message = (
+        <div>
+          <TopDisplay title={quiz[currentIndex].question} />
+          <AnswerFields
+            changed={this.answerChangedHandler}
+            clicked={this.answerSubmit}
+          />
+        </div>
+      );
+
+      // If the quiz is completed and not perfect,
+      // completed is displayed.
+    } else {
+      message = (
+        <div>
+          <TopDisplay title="Completed" />
+          You're score is: {answerCorrectly}/{quiz.length}
+        </div>
+      );
     }
 
-    answerSubmit = (e) => {
-        e.preventDefault();
-        e.target.reset();
-        const quiz = this.state.quiz
-        let currentIndex = this.state.currentIndex
-        let answerCorrectly = this.state.answerCorrectly
-        const currentAnswer = this.state.currentAnswer
-
-        if (currentAnswer === quiz[currentIndex].answer){
-            console.log('right')
-            this.setState({
-                currentIndex: currentIndex += 1,
-                answerCorrectly: answerCorrectly += 1})
-            console.log(currentIndex)
-        } else {
-            console.log('wrong')
-            this.setState({currentIndex: currentIndex += 1})
-        }
-
-        this.setState({currentAnswer: null})
-    }
-
-    render(){
-        const quiz = this.state.quiz
-        const currentIndex = this.state.currentIndex
-        const answerCorrectly = this.state.answerCorrectly
-        
-        let message = null
-        // If a perfect score is achieved Perfect is displayed
-        if (answerCorrectly === quiz.length ) {
-            message = <TopDisplay title="Perfect"/>
-
-        // If the current index is less than the quiz length,
-        // the quiz continues
-
-        } else if (currentIndex < quiz.length){
-            message = 
-            <div>                
-            <TopDisplay title={quiz[currentIndex].question}/>
-            <AnswerFields changed={this.answerChangedHandler} clicked={this.answerSubmit}/>
-            </div>
-
-        // If the quiz is completed and not perfect,
-        // completed is displayed.
-
-        } else {
-            message = <TopDisplay title="Completed"/>
-            
-        }
-        
-        return (
-            <div>
-                {message}
-            </div>
-        )
-    }
+    return (
+      <div>
+        {message}
+      </div>
+    );
+  }
 }
 
-export default BaseQuiz
+export default BaseQuiz;
